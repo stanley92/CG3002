c11079a5bc6240cf3a48909c09d8aee388f6dec8
stanley92 <Stanley92@hotmail.com>
2015-09-15 15:38:00 +0800

    Testing JsonData

--- JsonRsp.py
+++ JsonRsp.py
@@ -1,6 +1,7 @@
 import requests
 import json
 
+'''
 url = requests.get("http://showmyway.comp.nus.edu.sg/getMapInfo.php?Building=DemoBuilding&Level=2")
 resp = url.json()
 print(resp)
@@ -23,6 +24,7 @@ print ("")
 print ("")
 print ("")
 print ("response 2")
+'''
 
 url2 = "http://showmyway.comp.nus.edu.sg/getMapInfo.php"
 params = dict(
@@ -32,9 +34,32 @@ params = dict(
 
 resp2 = requests.get(url=url2, params=params)
 data = resp2.json()
-obj2 = json.dumps(data, dict, indent=4, sort_keys= True, separators=(',', ':'))
+#obj2 = json.dumps(data, dict, indent=4, sort_keys= True, separators=(',', ':'))
+info = data['info']
+details = data['map']
+wifi = data['wifi']
 
-print (obj2)
+print(info)
+print("")
+print(details)
+print("")
+print(wifi)
+print("")
+print (type(data))
 
-print (type(obj2))
-	
+for x in info:
+	print info['northAt']
+
+for w in details:
+	print int(w['y'])
+	print int(w['x'])
+	print int (w['nodeId'])
+	print str (w['nodeName'])
+	print [int(link) for link in (str(w['linkTo']).split(','))]
+
+for x in wifi:
+	print int(x['y'])
+	print int(x['x'])
+	print int (x['nodeId'])
+	print str (x['nodeName'])
+	print str (x[])
