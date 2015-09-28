fad74921adc66f5bedde6fd167566fa1e77c70b5
chongwyb <chongwyb@gmail.com>
2015-09-28 21:04:25 +0800

    Updated the corrected file

--- src/path/user_orientation.py
+++ src/path/user_orientation.py
@@ -6,7 +6,7 @@ class Compass():
     # User's Offset in Degrees
     self.compass_value = 0
     # Fixed angle btw North to curr n next node
-    self.angle_NorthToNodes = 0
+    self.angleOfNodes = 0
 
   def setCompassValue(self, v):
     self.compass_value = v #arduino_compass_value()
@@ -14,7 +14,7 @@ class Compass():
   def getCompassValue(self):
     return self.compass_value
 
-  def setNorthToNodes(self,graph,currNodeId,nextNodeId):
+  def setAngleOfNodes(self,graph,currNodeId,nextNodeId):
     x_curr = graph.getVertex(currNodeId).x
     y_curr = graph.getVertex(currNodeId).y
     x_next = graph.getVertex(nextNodeId).x
@@ -24,42 +24,49 @@ class Compass():
     
     # 1st Quadrant
     if x_next > x_curr and y_next > y_curr:
-      self.angle_NorthToNodes = 90 - math.degrees(math.atan(y_vert/x_hori))
+      self.angleOfNodes = 90 - math.degrees(math.atan(y_vert/x_hori))
     # 2nd Quadrant
     elif x_next > x_curr and y_next < y_curr:
-      self.angle_NorthToNodes = math.degrees(math.atan(y_vert/x_hori)) + 90
+      self.angleOfNodes = math.degrees(math.atan(y_vert/x_hori)) + 90
     # 3rd Quadrant
     elif x_next < x_curr and y_next < y_curr:
-      self.angle_NorthToNodes = 270 - math.degrees(math.atan(y_vert/x_hori))
+      self.angleOfNodes = 270 - math.degrees(math.atan(y_vert/x_hori))
     # 4th Quadrant
     elif x_next < x_curr and y_next > y_curr:
-      self.angle_NorthToNodes = math.degrees(math.atan(y_vert/x_hori)) + 270
+      self.angleOfNodes = math.degrees(math.atan(y_vert/x_hori)) + 270
     elif x_next == x_curr and y_next > y_curr:
-      self.angle_NorthToNodes = 0
+      self.angleOfNodes = 0
     elif x_next == x_curr and y_next < y_curr:
-      self.angle_NorthToNodes = 180
+      self.angleOfNodes = 180
     elif x_next > x_curr and y_next == y_curr:
-      self.angle_NorthToNodes = 90
+      self.angleOfNodes = 90
     elif x_next < x_curr and y_next == y_curr:
-      self.angle_NorthToNodes = 270
+      self.angleOfNodes = 270
       
-  def getNorthToNodes(self):
-    return self.angle_NorthToNodes
+  def getAngleOfNodes(self):
+    return self.angleOfNodes
 
   def userOffset(self): # - angle , get -ve gives , +ve give
-    offset = self.angle_NorthToNodes - self.compass_value
+    offset = self.angleOfNodes - self.compass_value
+    offset_abs = math.fabs(offset)
     if offset < 0:
-      return 'turn left'#1 # Turn Left
+      if offset_abs < 180:
+        return 'turn left'
+      else:
+        return 'turn right'
     elif offset > 0:
-      return 'turn right'#2 # Turn Right
+      if offset_abs < 180:
+        return 'turn right'
+      else:
+        return 'turn left'
     else:
-      return 'no turn'#0 # No turn
+      return 'no turn'
     
 ##x = Compass()
 ##g = get_map_info.generateGraph(get_map_info.getMapInfo('COM1',2))
-
+##
 ##x.setCompassValue(0)
 ##print('User angle = ' + str(x.getCompassValue()))
-##x.setNorthToNodes(g,5,4)
-##print('Node angle = ' + str(x.getNorthToNodes()))
+##x.setAngleOfNodes(g,5,4)
+##print('Node angle = ' + str(x.getAngleOfNodes()))
 ##print(x.userOffset())
