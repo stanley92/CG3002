11e4307840dcb15b80ec9e7cad4b5aba4d5052fe
chongwyb <chongwyb@gmail.com>
2015-10-04 00:10:01 +0800

    Modularise and bug fix
    Combined generate path to either use start or x,y.
    Walk takes in -1 if x,y value is given.
    Initial distance is always set as zero since a new object is created.
    Distance and angle now take in float values.

--- src/path/run_simulation.py
+++ src/path/run_simulation.py
@@ -24,8 +24,7 @@ class Simulation():
     self.y = y
     self.end = end
     self.graph = get_map_info.generateGraph(get_map_info.getMapInfo(building, level))
-    if start!= None:
-      self.path = self._generatePath()
+    self.path = self._generatePath()
     self.orient = user_orientation.Compass()
     self.orient.setNorthAt(heading)
     self.displace = user_displacement.Displacement()
@@ -54,6 +53,9 @@ class Simulation():
   def getEnd (self): 
     return self.end
 
+  ################################################
+  # Determines if the path is VALID
+  ################################################
   def _checkValidPath (self,graph):
     if not 0<self.start<=len(self.graph.getVertices()):
       print('No such start point')
@@ -63,54 +65,78 @@ class Simulation():
       return False
     return True
 
+  ################################################
+  # Assigns the path dependent on start or x,y
+  ################################################
   def _generatePath (self):
-    if self._checkValidPath(self.graph):
-      self.path = find_shortest_path.shortest(self.graph, self.start, self.end)
+    if self.start!=None:
+      if self._checkValidPath(self.graph):
+        self.path = find_shortest_path.shortest(self.graph, self.start, self.end)
+        return self.path
+    elif self.x!=None and self.y!= None:
+      closestVertex = get_map_info.getNearestVertex(self.x, self.y, self.graph)
+      self.path = find_shortest_path.shortest(self.graph, closestVertex.id, self.end)
       return self.path
     else:
       return None
 
+  ################################################
+  # Begin the navigation process
+  ################################################
   def start_nav(self):
-    if self.x!=None and self.y!= None:
-      closestVertex = get_map_info.getNearestVertex(self.x, self.y, self.graph)
-      self.path = find_shortest_path.shortest(self.graph, closestVertex.id, self.end)
-      
+          
     self.navigate()
 
-    print('You have reached your destination')
+    print('You have reached your destination!')
 
+  ################################################
+  # Tells the User to turn L/R before walking
+  ################################################
   def navigate(self):
     if self.x!=None and self.y!= None:
-      self.orient.setAngleOfNodeXY(self.graph,self.x,self.y,self.path[0])      
+      self.walk(-1)
       
     length = (len(self.path)) - 1
-    #self.displace.setDistTra(0)
-
+    
     for i in range(length):
+      self.walk(i)
       
+  ################################################
+  # Walking algoritim
+  # When i = -1, specially for random x,y location
+  # When i = 0 to path length, runs normally
+  # Calls the User to turn 1st before walking
+  ################################################
+  def walk(self, i):
+    if (i == -1):
+      self.orient.setAngleOfNodeXY(self.graph,self.x,self.y,self.path[0])
+      new_dist = (get_map_info._calcDistance(self.x, self.y, self.graph.getVertex(self.path[0]).x, self.graph.getVertex(self.path[0]).y))
+      self.displace.setDistCal(new_dist)
+    else:      
       self.orient.setAngleOfNodes(self.graph,self.path[i],self.path[i+1])
       old_dist = self.displace.getDistCal()
       new_dist = (get_map_info._calcDistance(self.graph.getVertex(self.path[i]).x, self.graph.getVertex(self.path[i]).y, self.graph.getVertex(self.path[i+1]).x, self.graph.getVertex(self.path[i+1]).y))
-      #print('new distance: ' + str(new_dist))
       total_dist = old_dist + new_dist
       self.displace.setDistCal(total_dist)
 
-      while(self.displace.getDistTra() < self.displace.getDistCal()):
-        # turning into correct direction
-        self.turn()
-        # distance to next node
-        print('Distance total: ' + str(self.displace.getDistCal()))
-        print('Distance travelled: ' + str(self.displace.getDistTra()))
-        print('Distance to next node: ' + str(self.displace.getDistCal()-self.displace.getDistTra()))
-        newDistTra = int(input('Enter extra distance travelled: '))
-        self.displace.setDistTra(self.displace.getDistTra() + newDistTra)
-        
+    while(self.displace.getDistTra() < self.displace.getDistCal()):
+      # turning into correct direction
+      self.turn()
+      # distance to next node
+      print('Distance total: ' + str(self.displace.getDistCal()))
+      print('Distance travelled: ' + str(self.displace.getDistTra()))
+      print('Distance to next node: ' + str(self.displace.getDistCal()-self.displace.getDistTra()))
+      newDistTra = float(input('Enter extra distance travelled: '))
+      self.displace.setDistTra(self.displace.getDistTra() + newDistTra)
 
+  ################################################
+  # Turning algorithim
+  ################################################
   def turn(self):
     while not(self.orient.getAngleOfNodes() - ANGLE_MARGIN <= self.orient.getCompassValue() <= self.orient.getAngleOfNodes() + ANGLE_MARGIN):
       #### TODO: handledata compassValue (keep reading)
       #### DUMMY: compass Value
-      newCompassData = int(input('compassData is (should turn to '+ str(self.orient.getAngleOfNodes()) +') :'))
+      newCompassData = float(input('compassData is (should turn to '+ str(self.orient.getAngleOfNodes()) +'): '))
       self.orient.setCompassValue(newCompassData)
       ####
       #### TODO: motor buzz
@@ -122,8 +148,11 @@ class Simulation():
     #print(self.orient.userOffset())
 
     #### TODO: motor
+
      
-    
+################################################
+# Simulated Event
+################################################
 while(1):
   print("Welcome")
   building = str(input('Building Name: '))
@@ -137,7 +166,7 @@ while(1):
   elif point == 'n':
     x_coord = int (input ('Input x-coordinate: '))
     y_coord = int (input ('Input y-coordinate: '))
-    heading = int (input ('Input heading: '))
+    heading = float (input ('Input heading: '))
     end = int(input('End: '))
     run = Simulation(building, level, x=x_coord, y=y_coord, end=end, heading = heading)
     run.start_nav()
