29a6cff9a0399ca79f9ea32f512f03e6b38bddf9
chongwyb <chongwyb@gmail.com>
2015-10-03 18:54:47 +0800

    Fix bug for node to node traversing
    Only works with starting node

--- src/path/run_simulation.py
+++ src/path/run_simulation.py
@@ -15,16 +15,18 @@ import math
 ANGLE_MARGIN = 5
 
 class Simulation():
-  def __init__ (self, building, level, start=None, x=None, y=None, end=None):
+  def __init__ (self, building, level, start=None, x=None, y=None, end=None, heading=0):
     self.building = building
     self.level = level
     self.start = start
     self.x = x
     self.y = y
     self.end = end
-    self.self.graph = get_map_info.generateGraph(building, level)
-    self.path = self._generatePath()
+    self.graph = get_map_info.generateGraph(get_map_info.getMapInfo(building, level))
+    if start!= None:
+      self.path = self._generatePath()
     self.orient = user_orientation.Compass()
+    self.orient.setNorthAt(heading)
     self.displace = user_displacement.Displacement()
 
   def setBuilding (self, building): 
@@ -52,7 +54,7 @@ class Simulation():
     return self.end
 
 
-  def _checkValidPath (self):
+  def _checkValidPath (self,graph):
 
     if not 0<self.start<=len(self.graph.getVertices()):
       print('No such start point')
@@ -69,46 +71,44 @@ class Simulation():
     else:
       return None
 
-  def start(self):
-    if x!=None and y!= None:
-      closestVertex = get_map_info.getNearestVertex(coordinate_x, coordinate_y, self.graph)
-      path = find_shortest_path.shortest(self.graph, closestVertex, self.end)
-    elif start != None:
-      path = self.path
-    else:
-      return None
-    return path
+  def start_nav(self):
+    if self.x!=None and self.y!= None:
+      closestVertex = get_map_info.getNearestVertex(self.x, self.y, self.graph)
+      self.path = find_shortest_path.shortest(self.graph, closestVertex, self.end)     
+    self.navigate()
 
   def navigate(self):
-    if x!=None and y!= None:
-      self.orient.setAngleOfNodeXY(graph,x_coord,y_coord,path[0])
-      length = (len(path)) - 1
-      self.displace.setDistTra(0)
+    if self.x!=None and self.y!= None:
+      self.orient.setAngleOfNodeXY(graph,self.x,self.y,self.path[0])
+      
+    length = (len(self.path)) - 1
+    self.displace.setDistTra(0)
 
-    for i in range (1,length):
+    for i in range(length):
 
-      self.orient.setAngleOfNodes(graph,path[i],path[i+1])
-      old_value = self.displace.getDistCal()
-      new_value = (get_map_info._calcDistance(graph.getVertex(i).x, graph.getVertex(i+1).x, graph.getVertex(i).y, graph.getVertex(i+1).y))
-      total_value = old_value + new_value
-      self.displace.setDistCal(total_value)
+      self.orient.setAngleOfNodes(self.graph,self.path[i],self.path[i+1])
+      old_dist = self.displace.getDistCal()
+      
+      new_dist = (get_map_info._calcDistance(self.graph.getVertex(self.path[i]).x, self.graph.getVertex(self.path[i]).y, self.graph.getVertex(self.path[i+1]).x, self.graph.getVertex(self.path[i+1]).y))
+      print('new distance: ' + str(new_dist))
+      total_dist = old_dist + new_dist
+      self.displace.setDistCal(total_dist)
 
       while(self.displace.getDistTra() < self.displace.getDistCal()):
-              # turning into correct direction
-              self.turn()
-
-              # walk
-              newDistTra = int(input('distance total' + str(distance) + ', enter distance travelled: '))
-              self.displace.setDistTra(newDistTra)
+        # turning into correct direction
+        self.turn()
+        print('Distance total: ' + str(self.displace.getDistCal()))
+        print('Distance travelled: ' + str(self.displace.getDistTra()))
+        newDistTra = int(input('Enter extra distance travelled: '))
+        self.displace.setDistTra(self.displace.getDistTra()+newDistTra)
+        
 
 
   def turn(self):
-    while (self.orient.getAngleOfNodes() - ANGLE_MARGIN \
-    <= self.orient.getCompassValue() \
-    <= self.orient.getAngleOfNodes() + ANGLE_MARGIN):
+    while not(self.orient.getAngleOfNodes() - ANGLE_MARGIN <= self.orient.getCompassValue() <= self.orient.getAngleOfNodes() + ANGLE_MARGIN):
       #### TODO: handledata compassValue (keep reading)
       #### DUMMY: compass Value
-      newCompassData = int(input('compassData is (should turn to '+ self.orient.getAngleOfNodes() +') :'))
+      newCompassData = int(input('compassData is (should turn to '+ str(self.orient.getAngleOfNodes()) +') :'))
       self.orient.setCompassValue(newCompassData)
       ####
       #### TODO: motor buzz
@@ -116,15 +116,33 @@ class Simulation():
       direction = self.orient.userOffset()
       print(direction);
       ####
-
-
     #print('Node angle = ' + str(self.orient.getAngleOfNodes()))
     #print(self.orient.userOffset())
 
     #### TODO: motor
-
-
-
+     
+    
+while(1):
+  print("Welcome")
+  building = str(input('Building Name: '))
+  level = input('Building Level: ')
+  point = input('Are you on a starting point? ')
+  if point == 'y':
+    start = int(input('Start: '))
+    end = int(input('End: '))
+    run = Simulation(building, level, start=start, end=end)
+    run.start_nav()
+  elif point == 'n':
+    x_coord = int (input ('Input x-coordinate: '))
+    y_coord = int (input ('Input y-coordinate: '))
+    heading = int (input ('Input heading: '))
+    end = int(input('End: '))
+    run = Simulation(building, level, x=x_coord, y=y_coord, end=end, heading = heading)
+    print(type(run))
+    run.start_nav()
+  
+  print("")
+  
 # Simulation(building, level, start=3, end=6)
 # Simulation(building, level, x=200, y=600, end=6)
 # DON'T DO THIS:
