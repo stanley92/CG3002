c22a2f8713f014d339cfb4104bf205d259567c28
Nguyen Cao Nhat Linh <nhatlinh95@gmail.com>
2015-10-15 17:26:17 +0800

    [updated] comm

--- src/communication/__main__.py
+++ src/communication/__main__.py
@@ -1,18 +1,10 @@
-from . import arduino
-from . import serial_comm
-from . import data_buffer
-import threading
-
-def read_from_port(buf):
-  buf.buffer()
+from . import communication
+from communication import communication
 
 def main():
-  ser = serial_comm.SerialCommunication()
-  ard = arduino.Arduino(ser)
-  if ard.handshake():
-    buf = data_buffer.DataBuffer(5, ard)
-    thread = threading.Thread(target=read_from_port,args=[buf])
-    thread.start()
+  c = communication.Communication()
+  c.initialise()
+  return c
   
 if __name__ == '__main__':
   main()
