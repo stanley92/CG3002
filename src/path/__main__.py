e62cf6da35d8e60daacd5cf6e7c7c2d9c7f111b4
stanley92 <Stanley92@hotmail.com>
2015-10-15 21:12:06 +0800

    Edited simulation process

--- src/path/__main__.py
+++ src/path/__main__.py
@@ -1,14 +1,25 @@
-from . import find_shortest_path
-from . import get_map_info
+import run_simulation
 
 if __name__ == '__main__':
-  building = 'COM1'
-  level = 2
-  start = 1
-  end = 28
-  print('Test for map of ' + building + ' level ' + str(level) +'.')
-  print('Finding path from node ID ' + str(start) + ' to ' + str(end))
-  map_info = get_map_info.getMapInfo(building, level)
-  graph = get_map_info.generateGraph(map_info)
-  path = find_shortest_path.shortest(graph, start, end)
-  print([graph.getVertex(i).name for i in path])
+################################################
+# Simulated Event
+################################################
+  while(1):
+    print("Welcome")
+    building = 'com1' #str(input('Building Name: '))
+    level = 2 #input('Building Level: ')
+    point = 'y' #input('Are you on a starting point? ')
+    if point == 'y':
+      start = 10 #int(input('Start: '))
+      end = 16 #int(input('End: '))
+      run = run_simulation.Simulation(building, level, start=start, end=end)
+      run.start_nav()
+    elif point == 'n':
+      x_coord = int (input ('Input x-coordinate: '))
+      y_coord = int (input ('Input y-coordinate: '))
+      heading = float (input ('Input heading: '))
+      end = int(input('End: '))
+      run = run_simulation.Simulation(building, level, x=x_coord, y=y_coord, end=end, heading = heading)
+      run.start_nav()
+    
+    print("")
--- src/path/run_simulation.py
+++ src/path/run_simulation.py
@@ -160,28 +160,7 @@ class Simulation():
     print('')
 
      
-################################################
-# Simulated Event
-################################################
-while(1):
-  print("Welcome")
-  building = 'com1' #str(input('Building Name: '))
-  level = 2 #input('Building Level: ')
-  point = 'y' #input('Are you on a starting point? ')
-  if point == 'y':
-    start = 10 #int(input('Start: '))
-    end = 16 #int(input('End: '))
-    run = Simulation(building, level, start=start, end=end)
-    run.start_nav()
-  elif point == 'n':
-    x_coord = int (input ('Input x-coordinate: '))
-    y_coord = int (input ('Input y-coordinate: '))
-    heading = float (input ('Input heading: '))
-    end = int(input('End: '))
-    run = Simulation(building, level, x=x_coord, y=y_coord, end=end, heading = heading)
-    run.start_nav()
-  
-  print("")
+
   
 # Simulation(building, level, start=3, end=6)
 # Simulation(building, level, x=200, y=600, end=6)
