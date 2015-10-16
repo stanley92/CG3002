0aeb6bd3be955739303ea1fbdf9d05e5df4dffe1
stanley92 <Stanley92@hotmail.com>
2015-10-16 20:58:03 +0800

    rpigpio

--- src/control/obstacle_detector.py
+++ src/control/obstacle_detector.py
@@ -3,6 +3,7 @@ import random
 import time
 import os
 import subprocess
+import RPi.GPIO as GPIO
 
 # def poll_data_from_queue():
 # 	#poll data
@@ -17,48 +18,48 @@ def ObstacleDetector():
   def collisionWarningLeft(self): #1
   	if self.sensors.sensorLeft < 30 and self.sensors.sensorLeft != 0:
   		print ("There is obstacle on the left.")
-  		return 1
+  		GPIO.output(23, True)
   	else: 
-  		return 0
+      GPIO.output(23, False)
 
   def collisionWarningRight(self):
     if self.sensors.sensorRight < 30 and self.sensors.sensorRight != 0:
       print ("There is obstacle on the Right.")
-      return 1
-    else:
-      return 0
+      GPIO.output(24, True)
+    else: 
+      GPIO.output(24, False)
 
   def collisionWarningFront(self): #3
     if self.sensors.sensorFront < 30 and self.sensors.sensorFront != 0:
       print ("There is obstacle in front.")
-      return 1
-    else:
-      return 0
+      GPIO.output(22, True)
+    else: 
+      GPIO.output(22, False)
 
   def collisionWarningLeg(self): #4
     if self.sensors.sensorLeg < 30 and self.sensors.sensorLeg != 0:
       print ("Low lying obstacle.")
-      return 1
+      GPIO.output(27, True)
     else: 
-      return 0
+      GPIO.output(27, False)
 
   def inf_loop(self):
   	while (1):
   		#poll_data_from_queue()
-  		valueDown = random.randint (0,100)
-  		s.setSensorDown(valueDown)
+  		# valueDown = random.randint (0,100)
+  		# s.setSensorDown(valueDown)
   		
-  		valueLeft = random.randint (0,100)
-  		s.setSensorLeft(valueLeft)
+  		# valueLeft = random.randint (0,100)
+  		# s.setSensorLeft(valueLeft)
   		
-  		valueRight = random.randint (0,100)
-  		s.setSensorRight(valueRight)
+  		# valueRight = random.randint (0,100)
+  		# s.setSensorRight(valueRight)
   		
-  		valueFront = random.randint (0,100)
-  		s.setSensorFront(valueFront)
+  		# valueFront = random.randint (0,100)
+  		# s.setSensorFront(valueFront)
   		
-  		valueLeg = random.randint (0,100)
-  		s.setSensorLeg(valueLeg)
+  		# valueLeg = random.randint (0,100)
+  		# s.setSensorLeg(valueLeg)
   		
   		#print ("Value: " + str (value))
   		collisionWarningDown(s)
@@ -79,3 +80,8 @@ def ObstacleDetector():
   	t.start()
   	#t.delay(100)
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(23, GPIO.OUT, pull_up_down = GPIO.PUD_DOWN)
+GPIO.setup(24, GPIO.OUT, pull_up_down = GPIO.PUD_DOWN)
+GPIO.setup(27, GPIO.OUT, pull_up_down = GPIO.PUD_DOWN)
+GPIO.setup(22, GPIO.OUT, pull_up_down = GPIO.PUD_DOWN)
