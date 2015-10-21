fde3cb511b1025cfd0738406f0997700f2b7a99c
stanley92 <Stanley92@hotmail.com>
2015-10-21 19:51:43 +0800

    Fixed collisionWarningHand in obstacle_detector.py

--- src/control/obstacle_detector.py
+++ src/control/obstacle_detector.py
@@ -31,7 +31,7 @@ class ObstacleDetector():
     else: 
       GPIO.output(25, False)
 
-  def collisionWarningFront(self): # detect obstacle with hand
+  def collisionWarningHand(self): # detect obstacle with hand
     if self.sensors.sensor_hand < 100 and self.sensors.sensor_hand != 0:
       print ("There is obstacle in front.")
       GPIO.output(22, True)
