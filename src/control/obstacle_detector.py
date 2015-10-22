7322bc22a8175b14b9e11f133e1d22c7daf22fa5
Nguyen Cao Nhat Linh <nhatlinh95@gmail.com>
2015-10-22 17:47:00 +0800

    fix indentation again

--- src/control/obstacle_detector.py
+++ src/control/obstacle_detector.py
@@ -5,7 +5,7 @@ import subprocess
 import RPi.GPIO as GPIO
 
 # def poll_data_from_queue():
-# 	#poll data
+#   #poll data
 class ObstacleDetector():
   def __init__(self, prog_controller, sensorData):
     self.sensors = sensorData
@@ -17,10 +17,10 @@ class ObstacleDetector():
   #     self.say("Stairs in front")
 
   def collisionWarningLeftAnkle(self): # detect Low Obstacle on the left
-  	if self.sensors.sensor_left_ankle < 60: #and self.sensors.sensor_left_ankle != 0:
-  		#print ("Low lying obstacle on the left.")
-  		GPIO.output(23, True)
-  	else:
+    if self.sensors.sensor_left_ankle < 60: #and self.sensors.sensor_left_ankle != 0:
+      #print ("Low lying obstacle on the left.")
+      GPIO.output(23, True)
+    else:
       GPIO.output(23, False)
       
   def collisionWarningRightAnkle(self): # detect Low Obstacle on the right
@@ -53,28 +53,28 @@ class ObstacleDetector():
 
   def inf_loop(self):
    while True:
-		#poll_data_from_queue()
-		# valueDown = random.randint (0,100)
-		# s.setSensorDown(valueDown)
-		
-		# valueLeft = random.randint (0,100)
-		# s.setSensorLeft(valueLeft)
-		
-		# valueRight = random.randint (0,100)
-		# s.setSensorRight(wvalueRight)
-		
-		# valueFront = random.randint (0,100)
-		# s.setSensorFront(valueFront)
-		
-		# valueLeg = random.randint (0,100)
-		# s.setSensorLeg(valueLeg)
-		
-		#print ("Value: " + str (value))
-		self.collisionWarningRight()
-		self.collisionWarningRightAnkle()
-		self.collisionWarningHand()
-		self.collisionWarningLeft()
-		self.collisionWarningLeftAnkle()
+    #poll_data_from_queue()
+    # valueDown = random.randint (0,100)
+    # s.setSensorDown(valueDown)
+    
+    # valueLeft = random.randint (0,100)
+    # s.setSensorLeft(valueLeft)
+    
+    # valueRight = random.randint (0,100)
+    # s.setSensorRight(wvalueRight)
+    
+    # valueFront = random.randint (0,100)
+    # s.setSensorFront(valueFront)
+    
+    # valueLeg = random.randint (0,100)
+    # s.setSensorLeg(valueLeg)
+    
+    #print ("Value: " + str (value))
+    self.collisionWarningRight()
+    self.collisionWarningRightAnkle()
+    self.collisionWarningHand()
+    self.collisionWarningLeft()
+    self.collisionWarningLeftAnkle()
     time.sleep(0.1)
     if not self.prog_controller.is_program_running():
       print('ObstacleDetector stopped')
