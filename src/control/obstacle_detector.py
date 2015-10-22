488932ec6baa4ce5c6d4d7aaeed39b14f272dff3
Nguyen Cao Nhat Linh <nhatlinh95@gmail.com>
2015-10-22 17:44:06 +0800

    indenting

--- src/control/obstacle_detector.py
+++ src/control/obstacle_detector.py
@@ -21,8 +21,8 @@ class ObstacleDetector():
   		#print ("Low lying obstacle on the left.")
   		GPIO.output(23, True)
   	else:
-		GPIO.output(23, False)
- 
+      GPIO.output(23, False)
+      
   def collisionWarningRightAnkle(self): # detect Low Obstacle on the right
     if self.sensors.sensor_right_ankle < 60: #and self.sensors.sensor_right != 0:
       #print ("Low lying obstacle on the Right.")
@@ -37,14 +37,14 @@ class ObstacleDetector():
     else: 
       GPIO.output(22, False)
 
-  def collisionWarningLeft(self): 
+      def collisionWarningLeft(self): 
     if self.sensors.sensor_left < 60: #and self.sensors.sensor_left != 0:
       #print ("Obstacle on the left.")
       GPIO.output(27, True)
     else: 
       GPIO.output(27, False)
 
-  def collisionWarningRight(self): 
+      def collisionWarningRight(self): 
     if self.sensors.sensor_right < 60: #and self.sensors.sensor_left != 0:
       #print ("Obsatcle on the right.")
       GPIO.output(24, True)
@@ -52,34 +52,34 @@ class ObstacleDetector():
       GPIO.output(24, False)
 
   def inf_loop(self):
-  	while True:
-  		#poll_data_from_queue()
-  		# valueDown = random.randint (0,100)
-  		# s.setSensorDown(valueDown)
-  		
-  		# valueLeft = random.randint (0,100)
-  		# s.setSensorLeft(valueLeft)
-  		
-  		# valueRight = random.randint (0,100)
-  		# s.setSensorRight(wvalueRight)
-  		
-  		# valueFront = random.randint (0,100)
-  		# s.setSensorFront(valueFront)
-  		
-  		# valueLeg = random.randint (0,100)
-  		# s.setSensorLeg(valueLeg)
-  		
-  		#print ("Value: " + str (value))
-  		self.collisionWarningRight()
-  		self.collisionWarningRightAnkle()
-  		self.collisionWarningHand()
-  		self.collisionWarningLeft()
-  		self.collisionWarningLeftAnkle()
-      time.sleep(0.1)
-  		if not self.prog_controller.is_program_running():
-		    print('ObstacleDetector stopped')
-        break
-  
+   while True:
+		#poll_data_from_queue()
+		# valueDown = random.randint (0,100)
+		# s.setSensorDown(valueDown)
+		
+		# valueLeft = random.randint (0,100)
+		# s.setSensorLeft(valueLeft)
+		
+		# valueRight = random.randint (0,100)
+		# s.setSensorRight(wvalueRight)
+		
+		# valueFront = random.randint (0,100)
+		# s.setSensorFront(valueFront)
+		
+		# valueLeg = random.randint (0,100)
+		# s.setSensorLeg(valueLeg)
+		
+		#print ("Value: " + str (value))
+		self.collisionWarningRight()
+		self.collisionWarningRightAnkle()
+		self.collisionWarningHand()
+		self.collisionWarningLeft()
+		self.collisionWarningLeftAnkle()
+    time.sleep(0.1)
+    if not self.prog_controller.is_program_running():
+      print('ObstacleDetector stopped')
+      break
+      
   def say(self, message):
     subprocess.call('espeak -v%s+%s "%s" 2>/dev/null' % ('en-us', 'f4', message), shell=True) 
 
