5614e124c228d83ca70e226e9d9b721ab456335d
stanley92 <Stanley92@hotmail.com>
2015-10-24 12:30:16 +0800

    wrong thread stopped

--- src/__main__.py
+++ src/__main__.py
@@ -134,11 +134,6 @@ if __name__ == '__main__':
       elif keypad_data.signal_prog_reset():
         prog_controller.stop()
         try:
-          c.thread.join() #polling
-        except NameError:
-          print('Communication data buffer thread never started')
-          pass
-        try:
           run_simulation_thread.join() #run simulation
         except NameError:
           print('Run simulation thread never started')
@@ -147,11 +142,6 @@ if __name__ == '__main__':
           obstacle_detect_thread.join() #obs detect
         except NameError:
           print('ObstacleDetector thread never started')
-          pass
-        try:
-          data_poll_thread.join() #polling
-        except NameError:
-          print('Data polling thread never started')
           pass
         say ('Program reset done')
   except KeyboardInterrupt:
