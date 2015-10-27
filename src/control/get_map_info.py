6b98b19c5a3c0a3cf69a831009f60c8b2b49aeb8
Nguyen Cao Nhat Linh <nhatlinh95@gmail.com>
2015-10-27 16:26:53 +0800

    donot download map for building42

--- src/control/get_map_info.py
+++ src/control/get_map_info.py
@@ -154,7 +154,7 @@ def parseMapData(data):
 
 def getMapInfo(building, level):
   try:
-    assert(building != 42 and building != '42')
+    assert(building != 42 or building != '42')
     jsonObject = downloadMap(dict(Building=building, Level=level))
   except Exception as e:
     print(str(e))
