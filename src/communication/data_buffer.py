f5af7af871f4f09bf0afa351d411b187727d9821
stanley92 <Stanley92@hotmail.com>
2015-11-03 15:49:31 +0800

    Revert "try fix lag"
    This reverts commit b689a5f770c7a345430497723340cb06ed6ec940.

--- src/communication/data_buffer.py
+++ src/communication/data_buffer.py
@@ -3,7 +3,7 @@ import logging
 class DataBuffer():
   def __init__(self, num_queue, ard):
     self.queues = num_queue*[0]
-    for i in range(3):
+    for i in range(num_queue):
       self.queues[i] = deque()
     self.ard = ard
     
@@ -11,10 +11,7 @@ class DataBuffer():
   def push(self, channel, m):
     try:
       if (channel != None and m != None):
-        if (channel in range(3)):
-          self.queues[channel].append(m)
-        else:
-          self.queues[channel] = m
+        self.queues[channel].append(m)
     except IndexError:
       print("Index Error")
       print("channel = " + str(channel))
@@ -25,48 +22,27 @@ class DataBuffer():
 
   def pop(self, channel):
     try:
-      if (channel in range(3)):
-        return self.queues[channel].popleft()
-      else:
-        m = self.queues[channel]
-        self.queues[channel] = None
-        return m
+      return self.queues[channel].popleft()
     except IndexError:
       return None
 
   def last(self, channel):
     try:
-      if (channel in range(3)):
-        return self.queues[channel].pop()
-      else:
-        m = self.queues[channel]
-        self.queues[channel] = None
-        return m
+      message = self.queues[channel].pop()
+      return message
     except IndexError:
       return None
 
   def clear(self, channel):
-    if (channel in range(3)):
-      self.queues[channel].clear()
-    else:
-      self.queues[channel] = None
+    self.queues[channel].clear()
 
   def pop_all(self, channel):
     all_data = []
     while True:
       try:
-        if (channel in range(3)):
-          all_data.append(self.queues[channel].pop());
-        else:
-          m = self.queues[channel]
-          self.queues[channel] = None
-          return m
+        all_data.append(self.queues[channel].pop());
       except IndexError:
         return all_data
 
   def have_data(self, channel):
-    if (channel in range(3)):
-      return bool(self.queues[channel])
-    else:
-      return self.queues[channel] != None
-    
+    return bool(self.queues[channel])
